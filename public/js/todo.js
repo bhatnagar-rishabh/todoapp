1df4839bf1d6a45970f7203f4cb253273d07df70
RishabhBhatnagar <bhatnagarrishabh4@gmail.com>
2021-01-11 09:18:11 +0530

    Add Page Header - Todo Input Div
    Signed-off-by: RishabhBhatnagar <bhatnagarrishabh4@gmail.com>

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,1 +1,10 @@
 // put js for todo app here
+
+
+function doSomething(textarea) {
+    if (textarea.value.length > 5) {
+        textarea.rows = 50;
+    } else {
+        textarea.rows = 15;
+    }
+}
